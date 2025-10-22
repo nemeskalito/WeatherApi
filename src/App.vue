cc36c1c9832e145c957477656b98540b95947267
nemeskalito <borisenko.igor2021@yandex.ru>
2025-10-22 09:01:05 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в {{ city }}</p>
+    <input
+      type="text"
+      placeholder="Введите город"
+      v-on:input="city = $event.target.value"
+    />
+    <button>Получить погоду</button>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background-color: #1f0f24;
+  text-align: center;
+  color: white;
+}
+
+.wrapper h1 {
+  margin-top: 50px;
+}
+.wrapper p {
+  margin-top: 20px;
+}
+
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: none;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
+
+.wrapper input:focus {
+  border-bottom-color: #6e2d7d;
+}
+.wrapper button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
+}
+</style>
